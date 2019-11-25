77784bb199831e3338aacca1a6e0788edd2147b4
MohamedEl-Dakdouky <45735497+MOHAMEDELDAKDOUKY@users.noreply.github.com>
2019-11-25 03:50:55 +0200

    Update sys_info.py

--- sys_info.py
+++ sys_info.py
@@ -17,3 +17,23 @@ print('RAM: {0} GB'.format(system_ram))
 for i in range (len(computer.Win32_VideoController())):
     print('Graphics Card %d : {}'.format(gpu_info[i].Name) %(i))
     
+
+    
+    
+# you need the module dbus-python
+def get_graphic_card_properties():
+    import dbus
+    bus = dbus.SystemBus()
+    hal_manager_object = bus.get_object('org.freedesktop.Hal', '/org/freedesktop/Hal/Manager')
+    prop = 'pci.device_class'
+    for device in hal_manager_object.get_dbus_method('GetAllDevices', 'org.freedesktop.Hal.Manager')():
+        dev = bus.get_object('org.freedesktop.Hal', device)
+        interface = dbus.Interface(dev, dbus_interface='org.freedesktop.Hal.Device')
+        if interface.PropertyExists(prop):
+            if interface.GetProperty(prop) == 3:
+                # we return the properties of the first device in the list
+                # with a pci.device_class == 3 (should check if several such devs...
+                return interface.GetAllProperties()
+dic = get_graphic_card_properties()
+for key, value in dic.iteritems():
+    print("%s : %s" %(key, value))
